d33621fc689a338254c4574c5d39c5e329ed18c7
zhengmz-aliyun <zhengmz@flowsea.com>
2022-06-27 11:50:54 +0800

    first commit

---
+++ GooFlow.color.js
@@ -0,0 +1,19 @@
+﻿GooFlow.prototype.color={
+  main:"#20A0FF",
+  font:"#15428B",
+  node:"#efefef",
+  line:"#ccc",
+  lineFont:"#ff6600",
+  mark:"#FF0000",
+  markFont:"#fff",
+  mix:"#B6F700",
+  mixFont:"#777"
+};/*
+GooFlow.prototype.color={
+  main:"#A8C508",
+  node:"#DFE9B4",
+  line:"#A8C508",
+  mark:"#E98200",
+  mix:"#B6F700",
+  font:"#357425"
+};*/
---
+++ README.md
@@ -0,0 +1,11 @@
+# gooflow
+
+拖拽式流程处理
+
+支持 ie8+ 以上的浏览器
+
+其他类似的项目有:
+
+- <https://github.com/codeZDC/gooflow>
+- <https://github.com/huangjunsen/GooFlow>
+
